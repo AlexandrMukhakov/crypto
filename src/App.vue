9c231c4719fc485bd729f0996c3ce3c43628f53c
alexandr <xavi08@yandex.ru>
2022-10-05 13:51:13 +0300

    refaktoring

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,9 @@
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE" />
             </div>
-            <div 
-             class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <div>
               <span
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-                
-
               </span>
             </div>
             <div @click="getDataw"
@@ -42,15 +39,20 @@
       </section>
 
       <template v-if="tikers.length">
+        <div>
+          <button v-if="startPage > 1" @click="page = page - 1" class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Назад</button>
+          <button v-if="hasNextPage" @click="page = page + 1" class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Вперед</button>
+          <div>Фильтр: <input v-model="fillter"/></div>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
-        {{sell}}
+        {{selectedTiker}}
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
 
-           v-for="(t, index) in tikers" 
+           v-for="(t, index) in pagginateTikker" 
           :key="index" 
           @click="select(t)" 
-          :class="sell === t ? 'border-4': ''"
+          :class="selectedTiker === t ? 'border-4': ''"
 
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
             <div class="px-4 py-5 sm:p-6 text-center">
@@ -75,15 +77,15 @@
 
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
-        <section v-if="sell" class="relative">
+        <section v-if="selectedTiker" class="relative">
           <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-            {{sell.name}} - USD
+            {{selectedTiker.name}} - USD
           </h3>
           <div class="flex items-end border-gray-600 border-b border-l h-64">
-            <div v-for="(bar, index) in normalizeGraph()" :key="index" :style="{ height: `${bar}%` }"
+            <div v-for="(bar, index) in normalizeGraph" :key="index" :style="{ height: `${bar}%` }"
               class="bg-purple-800 border w-10"></div>
           </div>
-          <button @click="sell=null" type="button" class="absolute top-0 right-0">
+          <button @click="selectedTiker=null" type="button" class="absolute top-0 right-0">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
               xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
               viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
@@ -111,12 +113,25 @@ export default {
     return {
       tiker: '',
       tikers: [],
-      sell: null,
+      selectedTiker: null,
       graph: [],
+      page: 1,
+      fillter: ""
     }
   },
 
   created() {
+
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+    if (windowData.fillter) {
+      this.fillter = windowData.fillter
+    }
+    if (windowData.page) {
+      this.page = windowData.page
+    }
+
     const tikerData = localStorage.getItem('cryptonomicon');
     if (tikerData) {
       this.tikers = JSON.parse(tikerData)
@@ -127,6 +142,36 @@ export default {
     
   },
 
+  computed: {
+    startPage() {
+      return (this.page - 1) * 6;
+    },
+
+    endPage() {
+      return this.page * 6
+    },
+
+    filterTikers() {
+      return this.tikers.filter(t => t.name.includes(this.fillter));
+    },
+    
+    pagginateTikker() {
+      return this.filterTikers.slice(this.startPage, this.endPage);
+    },
+
+    hasNextPage() {
+     return this.filterTikers.length > this.endPage;
+    },
+
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+  },
+
   methods: {
 
     subscribeData(tikerName) {
@@ -135,7 +180,7 @@ export default {
         const data = await f.json();
         this.tikers.find(t => t.name === tikerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
 
-        if (this.sell?.name === tikerName) {
+        if (this.selectedTiker?.name === tikerName) {
           this.graph.push(data.USD)
         }
 
@@ -148,33 +193,51 @@ export default {
         name: this.tiker,
         price: "_"
       }
-      this.tikers.push(newTicker);
-
-      localStorage.setItem('cryptonomicon', JSON.stringify(this.tikers));
+      this.tikers.unshift(newTicker);
       this.subscribeData(newTicker.name);
     },
 
     select(tiker) {
-      this.sell = tiker
-      this.graph = []
+      this.selectedTiker = tiker
     },
 
-    handeTicker(removeTick, index) {
+    handeTicker(removeTick) {
       this.tikers = this.tikers.filter(t => t != removeTick);
-      const localTiker = localStorage.getItem('cryptonomicon');
-      localStorage.removeItem(localTiker[index])
+      if (this.selectedTiker === removeTick) {
+        this.selectedTiker = null
+      }
     },
 
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(
-        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
 
-    
   },
+
+  watch: {
+
+    tikers(newValue, endValue) {
+      console.log(newValue.length, endValue.length)
+      localStorage.setItem('cryptonomicon', JSON.stringify(this.tikers));
+    },
+
+    selectedTiker() {
+      this.graph = [];
+    },
+
+    pagginateTikker() {
+      if (this.pagginateTikker.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },  
+
+    fillter() {
+      this.page = 1;
+      window.history.pushState(null, document.title, 
+      `${window.location.pathname}?fillter=${this.fillter}&page=${this.page}`);
+    },
+    page () {
+      window.history.pushState(null, document.title, 
+      `${window.location.pathname}?fillter=${this.fillter}&page=${this.page}`);
+    }
+  }
 
   
   
